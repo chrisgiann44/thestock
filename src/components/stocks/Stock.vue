cccb25251c5b24a6b44afdda623c23ddddfa62e8
Christos Giannouris <chris1giann1@gmail.com>
2019-10-25 11:12:56 +0200

    fourth

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,16 +3,25 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    Name
-                    <small>(Price:PRICE)</small>
+                    {{stock.name}}
+                    <small>(Price:{{stock.price}})</small>
                 </h3>
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" />
+                    <input
+                        type="number"
+                        class="form-control"
+                        v-model="quantity"
+                        placeholder="Quantity"
+                    />
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success">Buy</button>
+                    <button
+                        class="btn btn-success"
+                        @click="buyStock"
+                        :disabled="quantity<=0 || !Number.isInteger(Number(quantity)) "
+                    >Buy</button>
                 </div>
             </div>
         </div>
@@ -20,5 +29,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+    props: ["stock"],
+    data() {
+        return {
+            quantity: null
+        };
+    },
+    methods: {
+        buyStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            };
+            this.quantity = null;
+        }
+    }
+};
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,12 @@
+export default [
+    { id: 1, name: "BMW", price: 110 },
+    { id: 2, name: "Google", price: 200 },
+    { id: 3, name: "Apple", price: 250 },
+    { id: 4, name: "Twitter", price: 80 },
+    { id: 5, name: "Generali", price: 90 },
+    { id: 6, name: "Cosco", price: 110 },
+    { id: 7, name: "OTE", price: 200 },
+    { id: 8, name: "Sasmung", price: 250 },
+    { id: 9, name: "Huawei", price: 180 },
+    { id: 10, name: "Nokia", price: 690 }
+]
